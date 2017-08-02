fa604631dc38338487e29872f7c0c62162905fd9
Larry Taylor and Max Scher <max@>
2017-08-02 10:35:10 -0700

    implement statement for temperature with fahrenheit calculation

--- js/weather-interface.js
+++ js/weather-interface.js
@@ -4,9 +4,13 @@ $(document).ready(function() {
   $("#location-entry").submit(function(event) {
     event.preventDefault();
     var city = $('#city').val();
+    var tempKelvin;
+    var tempFahrenheit;
     $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey)
-      .then(function(response) {
-      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    .then(function(response) {
+      tempKelvin = response.main.temp;
+      tempFahrenheit = parseInt(1.8 * (tempKelvin - 273) + 32);
+      $('.showWeather').html("The humidity in " + city + " is " + response.main.humidity + "%. <br> The temperature in your beloved city of " + city + " is " + tempFahrenheit + " degrees.");
     })
       .fail(function(error) {
       $('.showWeather').text(error.responseJSON.message);
