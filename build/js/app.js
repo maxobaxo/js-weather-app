1be8e6c21e597107cbc059523b0bedbdadf97b68
Larry Taylor and Max Scher <larry@>
2017-08-02 10:01:08 -0700

    add error message from JSON

--- build/js/app.js
+++ build/js/app.js
@@ -5,8 +5,12 @@ $(document).ready(function() {
   $("#location-entry").submit(function(event) {
     event.preventDefault();
     var city = $('#city').val();
-    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey, function(response) {
-      $('.showWeather').text("The current humidity in " + city + " is " + response.main.humidity + "%");
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey)
+      .then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    })
+      .fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
     });
   });
 });
